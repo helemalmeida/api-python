f207fc56ba38ac2f2eb9f14fed1d73eb8a6a8847
Helem Almeida <88632793+helemalmeida@users.noreply.github.com>
2025-05-16 11:12:41 -0300

    Create app.py
    Dados direto no código, sem conexão com o banco de dados. Executa direto no navegador

---
+++ app.py
@@ -0,0 +1,16 @@
+#from flask import Flask, jsonify, request
+
+#app = Flask(__name__)
+
+#produtos = [
+#    {'id': 1, 'produto': 'Limao'},
+#    {'id': 2, 'produto': 'Laranja'},
+#    {'id': 2, 'produto': 'Maça'}
+#]
+
+#@app.route('/produtos', methods=['GET'])
+#def obter_produtos():
+#    return jsonify(produtos)
+
+#if __name__ == '__main__':
+#    app.run(port=5000, host='localhost', debug=True)
