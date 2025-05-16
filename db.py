6143eb4dd041258eff5c874f43e9ec98413b44d0
Helem Almeida <88632793+helemalmeida@users.noreply.github.com>
2025-05-16 11:15:33 -0300

    Create db.py
    Adiciona função de conexão com PostgreSQL usando dotenv

---
+++ db.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv()
+
+def obter_conexao():
+    conn = psycopg2.connect(
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT'),
+        database=os.getenv('DB_NAME')
+    )
+    return conn
